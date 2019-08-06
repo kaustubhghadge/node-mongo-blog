766a80672aa7c1f9af76f3db9d0ed2bcc8926e4b
Kaustubh Ghadge <kaustubhghadge@outlook.com>
2019-08-05 19:30:22 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const CONNECTION_URL = "YOUR CONNECTION STRING WITH USERNAME AND PASSWORD";
+const DATABASE_NAME = "accounting_department";
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+
+/**POST*/
+app.post("/personnel", (request, response) => {
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+
+/**GET*/
+app.get("/personnel", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+/**GET:ID*/
+app.get("/personnel/:id", (request, response) => {
+    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("personnel");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
